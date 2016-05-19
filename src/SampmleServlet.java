e6ad1d5dbcf5fb7e759c2bac878baa57d05956bb
Hiroyoshi Matsuzaki <h.matsuzak@gmail.com>
2016-05-19 21:25:03 +0900

    SQL接続テスト

--- src/SampmleServlet.java
+++ src/SampmleServlet.java
@@ -1,9 +1,11 @@
 
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -30,9 +32,34 @@ public class SampmleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try{
+			Driver d = (Driver)Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+
+			String strConn = "jdbc:sqlserver://ybl49nyf62.database.windows.net:1433;database=BallGame;";
+			String strUser = "user=matsuzhi@ybl49nyf62;";
+			String strPass = "password=AshnYuk2";
+			String connUrl = strConn + strUser + strPass;
+			Connection con = d.connect(connUrl, new Properties());
+
+			Statement stmt = con.createStatement();
+
+			String SQL = "SELECT userID FROM T001_ユーザーマスター";
+
+			ResultSet rs = stmt.executeQuery(SQL);
+
+			while(rs.next()){
+				System.out.println(rs.getString("userId"));
+			}
+			rs.close();
+			stmt.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		String[] luckArray = {"Very Good", "Good", "Bad"};
+		/*String[] luckArray = {"Very Good", "Good", "Bad"};
 		int index = (int)(Math.random() * 3);
 		String luck = luckArray[index];
 
@@ -49,6 +76,7 @@ public class SampmleServlet extends HttpServlet {
 		out.println("<p>" + today + "の運勢は" + luck + "です。zzz</p>");
 		out.println("</body>");
 		out.println("</html>");
+		*/
 
 	}
 
