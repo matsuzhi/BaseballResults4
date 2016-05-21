4a8bc419ccf5aacd015226f68bb39bdd31b9e14b
Hiroyoshi Matsuzaki <h.matsuzak@gmail.com>
2016-05-21 19:52:41 +0900

    ログイン方法変更
    ログインIDとパスワードの組をデータベースから取得するよう変更。
    パスワードはハッシュ化している。

--- src/model/LoginLogic.java
+++ src/model/LoginLogic.java
@@ -1,25 +1,67 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class LoginLogic {
 
 	private static String userId;
 	private static String userPass;
 	private static String errorMsg;
 
+	//ハッシュ化したパスワード
+	private static String hashPass;
 
 	public static void LoginCheck(LoginInput input){
 		userId = input.getuserId();
 		userPass = input.getuserPass();
 
-		//文字列の値が同じか見る場合はequalsメソッドを使う
-		//"=="は文字列の場合、同じオブジェクトかどうかを判別する
-		if(userId.equals("matsuzhi") &&  userPass.equals("test")){
-			errorMsg = "";
+		hashPass = SafePassword.getStretchedPassword(userPass, userId);
+
+
+		try{
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+			//接続文字列を作成（Azureポータルから持ってきただけ）
+			String connUrl = "jdbc:sqlserver://ybl49nyf62.database.windows.net:1433;";
+			connUrl = connUrl + "database=BallGame;user=matsuzhi@ybl49nyf62;password=AshnYuk2;";
+			connUrl = connUrl + "encrypt=true;trustServerCertificate=false;";
+			connUrl = connUrl + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+			//データベース接続
+			Connection con = DriverManager.getConnection(connUrl);
+
+			//エスケープしたSQL発行
+			String SQL = "SELECT COUNT(*) AS CNT FROM dbo.T001_ユーザーマスター WHERE userId = ? AND userPass = ?;";
+			PreparedStatement pstmt = con.prepareStatement(SQL);
+			pstmt.setString(1, userId);
+			pstmt.setString(2, hashPass);
+
+			ResultSet rs = pstmt.executeQuery();
+			int matchedRecordCount = 0;
+			while(rs.next()){
+				matchedRecordCount = rs.getInt("CNT");
+			}
+
+			rs.close();
+			pstmt.close();
+			con.close();
+
+
+
+			//ユーザーIDとパスワードが一致する組が存在したらログイン成功
+			if(matchedRecordCount == 1){
+				errorMsg = "";								//ログイン成功の場合、エラーメッセージは空白
+			}
+			else{
+				errorMsg = "ログインできませんでした。";	//ログイン失敗の場合はエラーメッセージを出さない
+			}
+
+			input.seterrorMsg(errorMsg);
 		}
-		else{
-			errorMsg = "ログインできませんでした。";
+		catch(Exception e){
+			e.printStackTrace();
 		}
-		//ログイン成功の場合、エラーメッセージは空白にする
-		input.seterrorMsg(errorMsg);
 	}
 }
